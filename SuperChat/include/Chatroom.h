e5544901f7c7a32deba22a208856f126cf43d87a
Matthew McLeod <matt.kent.mcleod@gmail.com>
2017-04-09 20:28:50 -0500

    partially built out Chatroom

--- SuperChat/include/Chatroom.h
+++ SuperChat/include/Chatroom.h
@@ -14,18 +14,25 @@ using namespace std;
 class Chatroom {
     private:
         ChatDaemon* daemon;
+        struct chatroom* chatroom_struct;
         string name;
+        int chatroom_idx;
         DDSEntityManager em;
         chatroomDataWriter_var Writer;
-        chatroomDataWriter_var Reader;
+        //chatroomDataWriter_var Reader;
         vector<User> users;
+        vector<Message> messages;
     public:
-        Chatroom(string name, ChatDaemon* daemon);
+        Chatroom(string passed_name, int passed_chatroom_idx,  ChatDaemon* passed_daemon);
+        Chatroom(struct chatroom* passed_chatroom_struct, ChatDaemon* passed_daemon);
+        ~Chatroom();
+        void setEntityManager();
+        void makeNewChatroom();
         void postMessage();
         void addUser(User* new_user);
         void sendMessage(Message* new_message);
         void recieveMessage(); //some params I need to figure out
-        
+        void sendChatroom();     
 };
 
 #endif
--- SuperChat/src/Chatroom.cpp
+++ SuperChat/src/Chatroom.cpp
@@ -1,5 +1,66 @@
 #include "../include/Chatroom.h"
 #include "../include/ChatDaemon.h"
 
-//stuff
 
+Chatroom::Chatroom(string passed_name, int passed_chatroom_idx, ChatDaemon* passed_daemon) {
+    name = passed_name; /**/
+    chatroom_idx = passed_chatroom_idx;
+    daemon = passed_daemon;
+
+    setEntityManager();
+    makeNewChatroom();
+       
+
+}
+
+Chatroom::Chatroom(struct chatroom* new_chatroom, ChatDaemon* passed_daemon) {
+    string name_temp(new_chatroom->chatroom_name);
+    name = name_temp;
+    chatroom_idx = new_chatroom->chatroom_idx;
+    chatroom_struct = new_chatroom;
+    
+    daemon = passed_daemon;
+
+    setEntityManager();
+}
+
+
+Chatroom::~Chatroom() {
+    em.deleteWriter();
+    em.deletePublisher();
+    em.deleteSubscriber();
+    em.deleteTopic();
+    em.deleteParticipant();
+}
+
+void Chatroom::setEntityManager() {
+    char* topic_name = "chatroom";
+    em.createParticipant("");
+
+    chatroomTypeSupport_var T = new chatroomTypeSupport();
+    em.registerType(T.in());
+    em.createTopic(topic_name); // I thought this was better than passing in the topic name
+    em.createPublisher();
+
+    em.createWriter(false);
+    DataWriter_var dwriter = em.getWriter();
+    Writer = chatroomDataWriter::_narrow(dwriter.in());
+    
+    //Is there no check for writer?
+    
+    em.createSubscriber();
+}
+
+
+void Chatroom::makeNewChatroom() {
+    struct chatroom new_chatroom;
+    strncpy(new_chatroom.chatroom_name, name.c_str(), sizeof(new_chatroom.chatroom_name));
+    new_chatroom.chatroom_idx = chatroom_idx;
+    chatroom_struct = &new_chatroom;
+}
+
+
+void Chatroom::sendChatroom() {
+    ReturnCode_t status = Writer->write(*chatroom_struct, HANDLE_NIL);
+    checkStatus(status, "Writer::write");
+}
